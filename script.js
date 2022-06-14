f3a43c351c75497312e967ef14091368ab3aa38c
toby king <tobyjacquesking@gmail.com>
2022-06-13 20:48:23 -0400

    done

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// TODO: add code here
+window.addEventListener("load", function(){
+
+    let json = [];
+    let index = 0;
+    
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then(function(json) {
+            const astronauts = document.getElementById("container");
+            for (index; index < json.length; index++) {
+                astronauts.innerHTML += `
+                <div class="astronaut">
+                    <div class="bio">
+                        <h3>${json[index].firstName} ${json[index].lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${json[index].hoursInSpace}</li>
+                            <li>Active: ${json[index].active}</li>
+                            <li>Skills: ${json[index].skills}</li>
+                        </ul>
+                    </div>
+                    <img class="avatar" src="${json[index].picture}">
+                </div>`
+            }
+        }); 
+    });  
+});
